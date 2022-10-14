f8eff4f78f428f30f8b24089805fd583629379c1
andypieratt <pieratt.aw@gmail.com>
2022-10-14 12:58:31 -0600

    started mapping out the header and home components, and will beging to add contents shortly

---
+++ client/src/components/home/Home.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import "./home.css";
+import { useNavigate } from "react-router-dom";
+
+const Home = () => {
+  return;
+};
+
+export default Home;
